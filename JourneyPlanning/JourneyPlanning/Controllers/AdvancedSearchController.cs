2afa93ecd4d664232dfa69baefaba5e3ac2b425d
unknown <vignesh@.(none)>
2012-02-27 23:14:21 +0530

    Initial GUI using ASP.NET MVC3 and Twitter bootstrap <vignessh>

---
+++ JourneyPlanning/JourneyPlanning/Controllers/AdvancedSearchController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Ttl.Web.JourneyPlanning.Controllers
+{
+    public class AdvancedSearchController : Controller
+    {
+        //
+        // GET: /AdvancedSearch/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
